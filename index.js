7d1266a70542e72d558f916ed4ec5baf818c79de
s3bc40 <s.goncalvesclaro@gmail.com>
2020-12-13 19:48:40 +0100

    Commands guide bot : server, users, args. WIP

--- index.js
+++ index.js
@@ -11,11 +11,33 @@ client.once('ready', () => {
 })
 // Listening for messages where bot has access
 client.on('message', (message) => {
+    // If no prefix or the message is from the bot exit
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
     // Command to execute and send back message
-    if(message.content.startsWith(`${prefix}pourparler`)) {
+    if (message.content.startsWith(`${prefix}pourparler`)) {
         message.channel.send('C\'est la loi, parle!');
-    } else if (message.content.startsWith(`${prefix}rhum`)) {
+    } 
+    else if (message.content.startsWith(`${prefix}rhum`)) {
         message.channel.send('Et une bouteille de Rhum!')
+    } 
+    else if (message.content === `${prefix}server`) {
+        const response = `Le nom du serveur où je réside est : ${message.guild.name}\nEquipage à bord : ${message.guild.memberCount}`; 
+        message.channel.send(response);
+    }
+    else if (message.content === `${prefix}user-info`) {
+        const response = `Ton nom : ${message.author.username}\nTon ID : ${message.author.id}`;
+        message.channel.send(response);
+    }
+    else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`Pas d\arguments ? SUS ${message.author.username} !`);
+        }
+
+        message.channel.send(`Commande : ${command}\nArguments : ${args}`);
     }
     console.log(message.content)
 })
