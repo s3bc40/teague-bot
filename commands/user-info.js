3b503f6a5d62f5085a677522ce04bc0970ad59a3
s3bc40 <s.goncalvesclaro@gmail.com>
2020-12-19 22:08:37 +0100

    Revert to user info only on user author

--- commands/user-info.js
+++ commands/user-info.js
@@ -8,7 +8,7 @@ module.exports = {
 	// eslint-disable-next-line no-unused-vars
 	execute(message, args) {
 		const { MessageEmbed } = require('discord.js');
-		const user = message.author || message.mentions.users.first();
+		const user = message.author;
 		const embed = new MessageEmbed()
 			.setTitle('Ta carte de pirate')
 			.setAuthor(user.username, user.displayAvatarURL({ format: 'png', dynamic: true }))
